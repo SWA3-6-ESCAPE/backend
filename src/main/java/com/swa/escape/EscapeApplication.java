2c67b3ac9defdecbb56bd09df7f273dd5bdea19b
bong-u <bongudev@gmail.com>
2023-08-18 16:46:57 +0900

    feat : 새로운 Springboot 프로젝트

---
+++ src/main/java/com/swa/escape/EscapeApplication.java
@@ -0,0 +1,13 @@
+package com.swa.escape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swa/escape/EscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swa.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
