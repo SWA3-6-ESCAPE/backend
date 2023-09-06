275d75e7d0e5598160e51cc77e365c7f1cad64fb
bong-u <bongudev@gmail.com>
2023-09-06 14:05:14 +0900

    feat : EventRepository 구현, 테스트

---
+++ src/main/java/com/swa/escape/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package com.swa.escape.repository;
+
+import com.swa.escape.domain.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
