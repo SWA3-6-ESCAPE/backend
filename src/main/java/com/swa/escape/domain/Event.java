5c56aa8be35648a76e8675c9e8b05dcad0841474
bong-u <bongudev@gmail.com>
2023-09-06 14:07:49 +0900

    fix : Report<->Event Cascade 설정

--- src/main/java/com/swa/escape/domain/Event.java
+++ src/main/java/com/swa/escape/domain/Event.java
@@ -1,5 +1,6 @@
 package com.swa.escape.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -34,10 +35,21 @@ public class Event {
     @Column(name = "event_latitude")
     private float latitude;
 
-
     @Column(name = "event_longitude")
     private float longitude;
 
-    @OneToMany(mappedBy = "event")
+    @Builder.Default
+    @OneToMany(mappedBy = "event", cascade= CascadeType.ALL)
     private List<Report> reports = new ArrayList<>();
+
+
+    public void addReport(Report report) {
+        reports.add(report);
+        report.setEvent(this);
+    }
+
+    public void removeReport(Report report) {
+        reports.remove(report);
+        report.setEvent(null);
+    }
 }
