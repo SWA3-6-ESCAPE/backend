0e19ee89c310f87f41568c813190ca9ee7adeedf
jin ho Jeon <jinhojeon@jins-MacBook-Pro.local>
2023-09-07 11:23:58 +0900

    feat : Rest API event 컨트롤러 생성

--- src/main/java/com/swa/escape/controller/EventController.java
+++ src/main/java/com/swa/escape/controller/EventController.java
@@ -1,12 +1,51 @@
 package com.swa.escape.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.swa.escape.domain.Event;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @RestController
 @RequestMapping("/api")
 public class EventController {
 
+    @PostMapping("/event/{id}")
+    public ResponseEntity<Event> createEvent(@RequestBody RequestBody requestBody) {
+        Event event = Event.builder()
+                .longitude(1.0F)
+                .latitude(1.0F)
+                .build();
+        return ResponseEntity.ok(event);
+    }
+
+    @GetMapping("/event/{id}")
+    public ResponseEntity<Event> getEvent(@PathVariable int id) {
+        Event event = Event.builder()
+                .longitude(1.0F)
+                .latitude(1.0F)
+                .build();
+        return ResponseEntity.ok(event);
+    }
+
+    @PatchMapping("/event/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable int id) {
+        Event event = Event.builder()
+                .longitude(1.0F)
+                .latitude(1.0F)
+                .build();
+        return ResponseEntity.ok(event);
+    }
+
+    @DeleteMapping("/event/{id}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable int id) {
+        Event event = Event.builder()
+                .longitude(1.0F)
+                .latitude(1.0F)
+                .build();
+        return ResponseEntity.ok(event);
+    }
 }
