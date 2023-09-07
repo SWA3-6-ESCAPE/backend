bb0b450cb87f6087423fc4b24ac20bc9f31b9591
JIN HO JEON <rhsek@naver.com>
2023-09-07 14:14:49 +0900

    fix : DTO 추가, 매핑 수정 그리고 Post 메소드 수정

--- src/main/java/com/swa/escape/controller/EventController.java
+++ src/main/java/com/swa/escape/controller/EventController.java
@@ -2,6 +2,7 @@ package com.swa.escape.controller;
 
 
 import com.swa.escape.domain.Event;
+import com.swa.escape.dto.EventCreateRequest;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,28 @@ import java.util.ArrayList;
 @RequestMapping("/api")
 public class EventController {
 
-    @PostMapping("/event/{id}")
-    public ResponseEntity<Event> createEvent(@RequestBody RequestBody requestBody) {
+    // Event 추가하는 API
+    @PostMapping("/event")
+    public ResponseEntity<Event> createEvent(@RequestBody EventCreateRequest eventRequest) {
+        Event event = Event.builder()
+                .longitude(eventRequest.getEvent_longitude())
+                .latitude(eventRequest.getEvent_longitude())
+                .build();
+        return ResponseEntity.ok(event);
+    }
+
+    @GetMapping("/event/{event_id}")
+    public ResponseEntity<Event> getEvent(@PathVariable int event_id) {
+        Event event = Event.builder()
+                .eventId(event_id)
+                .longitude(1.0F)
+                .latitude(1.0F)
+                .build();
+        return ResponseEntity.ok(event);
+    }
+
+    @PatchMapping("/event/{event_id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable int event_id) {
         Event event = Event.builder()
                 .longitude(1.0F)
                 .latitude(1.0F)
@@ -22,26 +43,8 @@ public class EventController {
         return ResponseEntity.ok(event);
     }
 
-    @GetMapping("/event/{id}")
-    public ResponseEntity<Event> getEvent(@PathVariable int id) {
-        Event event = Event.builder()
-                .longitude(1.0F)
-                .latitude(1.0F)
-                .build();
-        return ResponseEntity.ok(event);
-    }
-
-    @PatchMapping("/event/{id}")
-    public ResponseEntity<Event> updateEvent(@PathVariable int id) {
-        Event event = Event.builder()
-                .longitude(1.0F)
-                .latitude(1.0F)
-                .build();
-        return ResponseEntity.ok(event);
-    }
-
-    @DeleteMapping("/event/{id}")
-    public ResponseEntity<Event> deleteEvent(@PathVariable int id) {
+    @DeleteMapping("/event/{event_id}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable int event_id) {
         Event event = Event.builder()
                 .longitude(1.0F)
                 .latitude(1.0F)
