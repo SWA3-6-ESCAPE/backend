de94b28ec87744489bc9bcbfaeaf8bd4ce1141e4
JIN HO JEON <rhsek@naver.com>
2023-09-07 14:41:35 +0900

    refactor : 불필요한 것 수정

--- src/test/java/com/swa/escape/controller/EventControllerTest.java
+++ src/test/java/com/swa/escape/controller/EventControllerTest.java
@@ -1,27 +1,22 @@
 package com.swa.escape.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.swa.escape.domain.Event;
 import com.swa.escape.dto.EventCreateRequest;
-import com.swa.escape.dto.ReportModifyRequest;
-import jakarta.persistence.AssociationOverride;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,25 +35,28 @@ public class EventControllerTest {
     @Test
     @DisplayName("CREATE EVENT")
 
-    void createEvent() throws IOException, Exception{
+    void createEvent() throws Exception{
 
+        // given
         EventCreateRequest eventCreateRequest = new EventCreateRequest();
         eventCreateRequest.setEvent_latitude(1.0F);
         eventCreateRequest.setEvent_longitude(1.0F);
-//        eventController
+
+        // when
         mockMvc.perform(
                     post("/api/event")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(eventCreateRequest)))
+        // then
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.latitude").value(1.0F))
                 .andExpect(jsonPath("$.longitude").value(1.0F))
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
     }
 
     @Test
     @DisplayName("GET EVENT")
-    void getEvent() throws  Exception, IOException{
+    void getEvent() throws  Exception {
 
         // Given
         int event_id = 1;
@@ -70,15 +68,10 @@ public class EventControllerTest {
                 .andExpect(jsonPath("$.eventId").value(1))
                 .andExpect(jsonPath("$.latitude").value(1.0F))
                 .andExpect(jsonPath("$.longitude").value(1.0F))
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
     }
 
-    // 쓰지 않을 예정 -> Event를 수정하면 잘못된 정보가 갱신될 수도 있음.
-    //
-    //    @Test
-    //    void updateEvent() {
-    //
-    //    }
+
 
     @Test
     @DisplayName("DELETE EVENT")
@@ -93,6 +86,13 @@ public class EventControllerTest {
                 .andExpect(jsonPath("$.eventId").value(1))
                 .andExpect(jsonPath("$.latitude").value(1.0F))
                 .andExpect(jsonPath("$.longitude").value(1.0F))
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
     }
+
+    //    @Test
+    //    public void patchEventTest() throws Exception{
+    //        // given
+    //        int event_id = 1;
+    //
+    //    }
 }
