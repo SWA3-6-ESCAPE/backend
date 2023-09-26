cd0844c52e20a66631c24df47c4955d71ee5ad57
Song Ahyeon <ahyeonsong2001@gmail.com>
2023-09-26 11:07:52 +0900

    feat : EventService 생성

--- src/main/java/com/swa/escape/service/EventService.java
+++ src/main/java/com/swa/escape/service/EventService.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
+
+    // Event 생성
+    // 모든 Event 조회
+    // id로 Event 조회
+    // id로 Event 수정
+    // id로 Event 삭제
 }
