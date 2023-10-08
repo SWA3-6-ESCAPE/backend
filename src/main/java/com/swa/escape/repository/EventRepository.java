fb64f15c2df23fc727966c6153226aed23522a10
jinhojeon <rhsek@naver.com>
2023-10-08 21:56:31 +0900

    feat : EventController 연결을 위한 메소드 2개 생성(특정 이벤트 찾기, 특정 이벤트 불러오기)

--- src/main/java/com/swa/escape/repository/EventRepository.java
+++ src/main/java/com/swa/escape/repository/EventRepository.java
@@ -3,6 +3,13 @@ package com.swa.escape.repository;
 import com.swa.escape.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
+
+
+    public Event findEventByEventId(int eventId);
+
+    public List<Event> findAllEvents();
 }
