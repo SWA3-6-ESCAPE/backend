efe7a7665b118d690f4c603fa2551d98db502124
jinhojeon <rhsek@naver.com>
2023-10-09 01:03:09 +0900

    test : event Repository findAll

--- src/test/java/com/swa/escape/repository/EventRepositoryTest.java
+++ src/test/java/com/swa/escape/repository/EventRepositoryTest.java
@@ -2,15 +2,19 @@ package com.swa.escape.repository;
 
 import com.swa.escape.domain.Event;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @Slf4j
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class EventRepositoryTest {
+
   @Autowired
   private EventRepository eventRepository;
 
@@ -25,14 +29,43 @@ class EventRepositoryTest {
             .build();
 
     // When
+    System.out.println("event.getEventId() = " + event.getEventId());
     eventRepository.save(event);
-
     // Then
     Event savedEvent = eventRepository.findById(eventId).orElse(null);
+    System.out.println("event.getEventId() = " + savedEvent.getEventId());
     assert savedEvent != null;
     assert savedEvent.getEventId() == event.getEventId();
     assert savedEvent.getLatitude() == event.getLatitude();
     assert savedEvent.getLongitude() == event.getLongitude();
+  }
+
+  @Test
+  void findEventsTest(){
+
+    int eventId = 2;
+    Event event = Event.builder()
+            .eventId(eventId)
+            .latitude(1.0f)
+            .longitude(1.0f)
+            .build();
+
+    eventRepository.save(event);
+
+    Event event1 = Event.builder()
+            .eventId(++eventId)
+            .latitude(1.0f)
+            .longitude(1.0f)
+            .build();
+
+    eventRepository.save(event1);
+
+
+    List<Event> allEvents = eventRepository.findAllEvents();
+
+    Assertions.assertThat(allEvents.get(0).getEventId()).isEqualTo(1);
+    System.out.println("EventRepositoryTest.findEventsTest");
+    Assertions.assertThat(allEvents.get(1).getEventId()).isEqualTo(2);
   }
 
 }
