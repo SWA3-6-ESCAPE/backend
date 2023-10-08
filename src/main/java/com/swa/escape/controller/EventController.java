9c1935132739e9d2d06131beeb16e75b6ec70c76
jinhojeon <rhsek@naver.com>
2023-10-08 23:29:20 +0900

    feat : event-controller와 eventRepository 연결

--- src/main/java/com/swa/escape/controller/EventController.java
+++ src/main/java/com/swa/escape/controller/EventController.java
@@ -3,7 +3,7 @@ package com.swa.escape.controller;
 
 import com.swa.escape.domain.Event;
 import com.swa.escape.dto.EventCreateRequest;
-import org.springframework.http.RequestEntity;
+import com.swa.escape.repository.EventRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 @RequestMapping("/api")
 public class EventController {
 
+    private EventRepository eventRepository;
+
     // Event 추가하는 API
     @PostMapping("/event")
     public ResponseEntity<Event> createEvent(@RequestBody EventCreateRequest eventRequest) {
@@ -21,34 +23,35 @@ public class EventController {
                 .longitude(eventRequest.getEvent_longitude())
                 .latitude(eventRequest.getEvent_longitude())
                 .build();
+        eventRepository.save(event);
         return ResponseEntity.ok(event);
     }
 
     @GetMapping("/event/{event_id}")
     public ResponseEntity<Event> getEvent(@PathVariable int event_id) {
-        Event event = Event.builder()
-                .eventId(event_id)
-                .longitude(1.0F)
-                .latitude(1.0F)
-                .build();
-        return ResponseEntity.ok(event);
+        Event event = eventRepository.findEventByEventId(event_id);
+
+        if(event != null){
+            return ResponseEntity.ok(event);
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @PatchMapping("/event/{event_id}")
-    public ResponseEntity<Event> updateEvent(@PathVariable int event_id) {
-        Event event = Event.builder()
-                .longitude(1.0F)
-                .latitude(1.0F)
-                .build();
-        return ResponseEntity.ok(event);
-    }
+    // 이부분은 사용할 일이 없어서 주석처리 했습니다.
+    // @PatchMapping("/event/{event_id}")
+    // public ResponseEntity<Event> updateEvent(@PathVariable int event_id) {
+    //     Event event = Event.builder()
+    //             .longitude(1.0F)
+    //             .latitude(1.0F)
+    //             .build();
+    //     return ResponseEntity.ok(event);
+    // }
 
     @DeleteMapping("/event/{event_id}")
     public ResponseEntity<Event> deleteEvent(@PathVariable int event_id) {
-        Event event = Event.builder()
-                .longitude(1.0F)
-                .latitude(1.0F)
-                .build();
+         Event event = eventRepository.findEventByEventId(event_id);
+        eventRepository.deleteById(event_id);
+        // redirect
         return ResponseEntity.ok(event);
     }
 }
